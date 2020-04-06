e2a261e3b61566e2e73b80182683bdbc4fa7e176
Sergey Fedotov <fsa@tavda.info>
2020-04-06 19:49:10 +0700

    Крупное обновление. Не совместимо с предыдущей версией.

--- classes/auth/userentity.php
+++ classes/auth/userentity.php
@@ -2,50 +2,65 @@
 
 namespace Auth;
 
-class UserEntity implements UserInterface {
-    
-    use jsonUnserialize;
+use DB;
+
+class UserEntity extends \Entity {
+
+    const TABLENAME='auth_users';
 
     public $id;
-    public $login='guest';
-    public $name='Гость';
+    public $login;
+    public $password;
+    public $name;
     public $email;
-    public $scope='["guest"]';
-    public $disabled=false;
+    public $scope;
+    public $ip_register;
+    public $registered;
+    public $activate_key;
+    public $activated;
+    public $updated;
+    public $disabled;
 
-    public function __sleep() {
-        return ['id','login','name','email','scope','disabled'];
-    }
-    
-    public function jsonSerialize() {
-        return [
-            'id'=>$this->id,
-            'login'=>$this->login,
-            'name'=>$this->name,
-            'email'=>$this->email,
-            'scope'=>$this->scope,
-            'disabled'=>$this->disabled
-        ];
+    public function __construct() {
+        $this->password=null;
+        if(!is_null($this->scope)) {
+            $this->scope=json_decode($this->scope);
+        }
     }
 
-    public function getId(): ?int {
+    public function memberOf($group) {
+        if (is_null($this->scope)) {
+            return false;
+        }
+        return array_search($group, $this->scope)!==false;
+    }
+
+    public function insert() {
+        $this->id=DB::insert(self::TABLENAME, [
+            "login"=>$this->login,
+            "password"=>password_hash($this->password, PASSWORD_DEFAULT),
+            "name"=>$this->name,
+            "email"=>$this->email,
+            "scope"=>is_null($this->scope)?'[]':json_encode($this->scope),
+        ]);
         return $this->id;
     }
-    
-    public function getLogin(): string {
-        return $this->login;
-    }
 
-    public function getName(): string {
-        return $this->name;
-    }
-
-    public function getEmail(): string {
-        return $this->email;
-    }
-
-    public function getScope(): array {
-        return json_decode($this->scope);
+    public function update() {
+        $this->updated=date('c');
+        $user=[
+            "id"=>$this->id,
+            "login"=>$this->login,
+            "name"=>$this->name,
+            "email"=>$this->email,
+            "scope"=>is_null($this->scope)?'[]':json_encode($this->scope),
+            "updated"=>$this->updated,
+            "disabled"=>$this->disabled?'t':'f'
+        ];
+        if(!is_null($this->password)) {
+            $user['password']=password_hash($this->password, PASSWORD_DEFAULT);
+        }
+        return DB::update(self::TABLENAME, $user);
     }
 
 }
